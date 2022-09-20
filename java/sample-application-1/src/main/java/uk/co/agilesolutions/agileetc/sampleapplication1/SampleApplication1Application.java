579cd1fffbed5126ca7023aec64d6afeab6685b1
devops <devops@localhost.localdomain>
2022-09-20 13:08:02 +0100

    Add sample java app

---
+++ java/sample-application-1/src/main/java/uk/co/agilesolutions/agileetc/sampleapplication1/SampleApplication1Application.java
@@ -0,0 +1,13 @@
+package uk.co.agilesolutions.agileetc.sampleapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication1Application.class, args);
+	}
+
+}
---
+++ java/sample-application-1/src/test/java/uk/co/agilesolutions/agileetc/sampleapplication1/SampleApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.agilesolutions.agileetc.sampleapplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplication1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
